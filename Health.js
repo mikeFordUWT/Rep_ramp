ad089f2632ea35953cf5e35f917b33ec73d48366
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-05-20 15:20:00 -0700

    for control work

--- Health.js
+++ Health.js
@@ -1,9 +1,15 @@
+/*
+ * Created by Chris
+ */
+var BAR_WIDTH = 500;
+var BUFFER = 10;
+var BAR_HEIGHT = 30;
 
 function Health(player, fighter) {
     this.healthPoints = 100;
     this.side = player;
     this.fighter = fighter;
-
+    this.background = 0;
 }
 
 Health.prototype = new Entity();
@@ -18,81 +24,87 @@ Health.prototype.draw = function (ctx) {
     var canvas = document.getElementById('gameWorld');
     this.ctx = ctx;
 
-    if(this.side === "left"){
-
+    //Initial Background setup
+    if(this.background === 0 && this.side === "left"){
         ctx.beginPath();
-        ctx.rect(15,15+15,510,40);
+        ctx.rect(0, 0, canvas.width, BAR_HEIGHT * 2);
+        ctx.fillStyle = "black";
+        ctx.fill();
+        ctx.closePath();
+        var img = document.getElementById("titleBattle");
+        ctx.drawImage(img, canvas.width/2-(img.width/4), BAR_HEIGHT/2, img.width/2, img.height/2);
+    }
+
+    //Left Player Logic
+    if(this.side === "left"){
+        //Bar Framed
+        ctx.beginPath();
+        ctx.rect(BUFFER*1.5, BAR_HEIGHT*1.5, BAR_WIDTH + BUFFER, BAR_HEIGHT + BUFFER);
         ctx.fillStyle = "blue";
         ctx.fill();
         ctx.closePath();
 
+        //Health
         ctx.beginPath();
-        ctx.rect(20,20+15,500,30);
+        ctx.rect(BUFFER*2, BUFFER*0.5 + BAR_HEIGHT*1.5, BAR_WIDTH, BAR_HEIGHT);
         ctx.fillStyle = "red";
         ctx.fill();
         ctx.closePath();
 
+        //Covering Dead Health
         if(this.healthPoints >= 0) {
             ctx.beginPath();
-            ctx.rect(520 - (100-this.healthPoints)*5,20+15, (100-this.healthPoints)*5 ,30);
-            ctx.fillStyle = "white";
+            ctx.rect(BAR_WIDTH + BUFFER*2 - (100-this.healthPoints)*5, (BUFFER*0.5)+BAR_HEIGHT*1.5, (100-this.healthPoints)*5 ,BAR_HEIGHT);
+            ctx.fillStyle = "blue";
             ctx.fill();
             ctx.closePath();
-
         } else {
             ctx.beginPath();
-            ctx.rect(20,35, 500 ,30);
-            ctx.fillStyle = "white";
+            ctx.rect(BUFFER*2, BUFFER*0.5+BAR_HEIGHT*1.5, BAR_WIDTH, BAR_HEIGHT);
+            ctx.fillStyle = "blue";
             ctx.fill();
             ctx.closePath();
         }
-
         //Display Names
         ctx.font = "30px Arial";
-        ctx.fillStyle = "black";
-        /*for(var i=0; i<this.fighter.fighter.length; i++){
-            if(this.fighter.fighter[i].match(/[A-Z]/) != null){
-                ctx.fillText(this.fighter.fighter.substring(i, this.fighter.fighter.length).toUpperCase(),10+10,30+5);
-            }
-        }*/
-        ctx.fillText(this.fighter.fighter,15,30);
+        ctx.fillStyle = "white";
+        ctx.fillText(this.fighter.fighter, BUFFER*3, BAR_HEIGHT*1.25);
 
-    } else {
+    } else { //Right Player Logic
+        //Outer Frame
         ctx.beginPath();
-        ctx.rect(canvas.width-300-20,10,310,40);
+        ctx.rect(canvas.width-BAR_WIDTH-(BUFFER*2), BAR_HEIGHT*1.5, BAR_WIDTH+BUFFER, BAR_HEIGHT+BUFFER);
         ctx.fillStyle = "blue";
         ctx.fill();
         ctx.closePath();
 
+        //Health
         ctx.beginPath();
-        ctx.rect(canvas.width-315,15,300,30);
+        ctx.rect(canvas.width-BAR_WIDTH-BUFFER*1.5, BUFFER*0.5+BAR_HEIGHT*1.5, BAR_WIDTH, BAR_HEIGHT);
         ctx.fillStyle = "red";
         ctx.fill();
         ctx.closePath();
 
+        //Covering Dead Health
         if(this.healthPoints >= 0) {
             ctx.beginPath();
-            ctx.rect(canvas.width-300-15,15,(100-this.healthPoints)*3,30);
-            ctx.fillStyle = "white";
+            ctx.rect(canvas.width-BAR_WIDTH-BUFFER*1.5, BUFFER*0.5+BAR_HEIGHT*1.5, (100-this.healthPoints)*5, BAR_HEIGHT);
+            ctx.fillStyle = "blue";
             ctx.fill();
             ctx.closePath();
         } else {
             ctx.beginPath();
-            ctx.rect(canvas.width-300-15,15,300,30);
-            ctx.fillStyle = "white";
+            ctx.rect(canvas.width-BAR_WIDTH-BUFFER*1.5, BUFFER*0.5+BAR_HEIGHT*1.5, BAR_WIDTH, BAR_HEIGHT);
+            ctx.fillStyle = "blue";
             ctx.fill();
             ctx.closePath();
         }
 
         //Display Name
         ctx.font = "30px Arial";
-        /*for(var i=0; i<this.fighter.fighter.length; i++){
-            if(this.fighter.fighter[i].match(/[A-Z]/) != null){
-                ctx.fillText(this.fighter.fighter.substring(i, this.fighter.fighter.length).toUpperCase(),canvas.width-325,30);
-            }
-        }*/
-        ctx.fillText(this.fighter.fighter,canvas.width-315,30);
-        //console.log(this.fighter.fighter);
+        ctx.fillStyle = "white";
+        ctx.fillText(this.fighter.fighter,canvas.width-(this.fighter.fighter.length*30),BAR_HEIGHT*1.25);
+
     }
 
     Health.prototype.update = function () {
